c7da2ac5027e7a70209895d2305fe28bcaaa80f2
yourname <409416541l@gms.tku.edu.tw>
2021-06-10 08:41:19 +0800

    adding END TERM PROJECT files

--- class demo/w13/script.js
+++
@@ -1,7 +0,0 @@
-const p = document.querySelector('.menu-btn');
-const mainMenu = document.querySelector('.main-nav .main-menu');
-
-// console.log('mainMenu' mainMenu)
-p.addEventListener('click', function () {
-    mainMenu.classList.toggle('show')
-})
